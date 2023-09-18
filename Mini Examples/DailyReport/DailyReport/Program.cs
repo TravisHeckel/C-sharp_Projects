0dd8766057d2637bfdfd09c8f5265d5246e02d1b
TravisHeckel <Traheck@gmail.com>
2023-09-18 16:46:59 -0600

    updated assignment

--- Mini Examples/DailyReport/DailyReport/Program.cs
+++ Mini Examples/DailyReport/DailyReport/Program.cs
@@ -27,20 +27,6 @@ namespace DailyReport
             int page = Convert.ToInt32(pageNumber);
             //Console.WriteLine("Your page number is: " + page  + '\n');//I used this to confirm the correct answers are given.
 
-            //------hours worked----------------------------------------------------------------------------------
-            Console.WriteLine("How many hours have you practiced today?" + '\n');
-            Console.WriteLine("please only type a whole number. Do NOT type out the word.");
-            string hoursWorked = Console.ReadLine();//This command will save what the user inputs.
-            bool check = int.TryParse(hoursWorked, out _);
-            
-            while (check != true)
-            {
-                Console.WriteLine("please only type a whole number. Do NOT type out the word.");
-                hoursWorked = Console.ReadLine();//This command will save what the user inputs.
-            }
-            int hour = Convert.ToInt32(hoursWorked);
-            //Console.WriteLine("Your hours today are  " + hour + '.' + '\n');  //I used this to confirm the correct answers are given.
-
 
             //-----display help question-------------------------------------------------------------------------------------
             Console.WriteLine("Do you need help with anything?");
@@ -65,6 +51,30 @@ namespace DailyReport
             //{
             //    Console.WriteLine("You respond with " + finalAnswer + ", keep up with the good work and have a wonderful day!");
             //}
+
+            //------positive experiences shared----------------------------------------------------------------------------------
+            Console.WriteLine("Were there any positive experiences you'd like to share?"); //This will ask for your input
+            string experience = Console.ReadLine(); //This command will save what the user inputs.
+            //Console.WriteLine("Your positive experience was: " + experience + '\n'); //I used this to confirm the correct answers are given.
+
+            //------any feedback shared----------------------------------------------------------------------------------
+            Console.WriteLine("Was there any feedback you would like to share?"); //This will ask about getting feedback?
+            string feedback = Console.ReadLine(); //This command will save what the user inputs.
+            //Console.WriteLine("Feedback: " + feedback + '\n'); //I used this to confirm the correct answers are given.
+
+            //------hours worked----------------------------------------------------------------------------------
+            Console.WriteLine("How many hours have you practiced today?" + '\n');
+            Console.WriteLine("please only type a whole number. Do NOT type out the word.");
+            string hoursWorked = Console.ReadLine();//This command will save what the user inputs.
+            bool check = int.TryParse(hoursWorked, out _);
+
+            while (check != true)
+            {
+                Console.WriteLine("please only type a whole number. Do NOT type out the word.");
+                hoursWorked = Console.ReadLine();//This command will save what the user inputs.
+            }
+            int hour = Convert.ToInt32(hoursWorked);
+            //Console.WriteLine("Your hours today are  " + hour + '.' + '\n');  //I used this to confirm the correct answers are given.
 
 
             Console.ReadLine(); //This displays all the information including questions.
