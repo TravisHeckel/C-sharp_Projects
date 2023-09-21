bfb59ca3da365ebfd2ae75a7729623d212121cd1
TravisHeckel <Traheck@gmail.com>
2023-09-20 19:58:09 -0600

    update string function notes

---
+++ Mini Examples/stringFunction/stringFunction/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Jesse";
+            //string quote = "The man said, \"I forgot what i wanted to say...\n \t well that doesnt matter anymore \".";
+            //string fileName = @"C:\user\jesse";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+            //Console.WriteLine(length);
+
+            //name = name.ToUpper();
+            //name = name.ToLower();
+
+
+            //Console.WriteLine(length);
+            //---------------------I concatenate three strings --------------------
+            string firstName = "travis";
+            string middleName = "James";
+            string lastName = "Heckel";
+            Console.WriteLine(firstName + middleName + lastName);
+            //----------------------Converted a string to uppercase---------------------
+            string firsttry = firstName.ToUpper();
+            Console.WriteLine(firsttry);
+
+            //--------------------Creates a string builder and builds a a paragraph of text, one sentence at a time.--------------------
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("my Name is Travis Heckel!");
+            sb.Append("I like food, and ");
+            sb.Append("I hate heat.");
+            sb.Append("I ran for president once.");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
