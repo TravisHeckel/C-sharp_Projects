a0ce34d19eef4fd1da35b2577c78a68f24a13635
TravisHeckel <Traheck@gmail.com>
2023-09-23 05:40:19 -0600

    lesson notes for how to do exceptions

--- Mini Examples/Exceptions/Exceptions/Program.cs
+++ Mini Examples/Exceptions/Exceptions/Program.cs
@@ -7,16 +7,33 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pick a number.");
-            int numberOne = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Pick a second number.");
-            int numberTwo = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Dividing the two numbers....");
-            int numberThree = numberOne / numberTwo;
-            Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+            try //this will work thru the system and if it hits an error drop down and try the catch to share the error
+            {
+                Console.WriteLine("Pick a number.");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number.");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two numbers....");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+            }
+            catch (FormatException ex) 
+            {
+                Console.WriteLine("Please type a whole number.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
 
-
-            Console.ReadLine();
         }
     }
 }
