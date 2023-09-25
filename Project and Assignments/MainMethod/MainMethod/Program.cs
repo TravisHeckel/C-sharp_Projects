742ca6003d3f3311d1452596acc4c81c8b295a6c
TravisHeckel <Traheck@gmail.com>
2023-09-25 12:44:38 -0600

    finished assignment
    I created and completed the assignment for the main method.
    
    fixed and reuploaded a corrupt file.

---
+++ Project and Assignments/MainMethod/MainMethod/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathOP mutli = new mathOP(); //calls a instance of the class.
+            
+            int Number1 = 6; //sets our  variable
+            Console.WriteLine(Number1 + " times 10 equals " + mathOP.MathOperation(Number1) + '\n');
+
+            decimal Number2 = 6.99m; //sets our  variable
+            Console.WriteLine("$"+ Number2 + " + $1.71 for gas money means you owe me $" + mathOP.MathOperation(Number2) + '\n');
+
+            //string Number3 = "c"; //sets our  variable
+            string Number3 = "8"; //sets our  variable
+            int result = mathOP.MathOperation(Number3); //sets our answer after running the method
+
+            if (result > 0) //checks to see if 0 is passed as an error
+            {
+                Console.WriteLine(Number3 + " divided by 4 equals " + mathOP.MathOperation(Number3) + '\n');
+            }
+            else
+            {
+                Console.WriteLine("Sorry the input you gave does not provide an appropriate number." + '\n');
+            }
+
+            Console.ReadLine();
+        }
+    
+    }
+}
---
+++ Project and Assignments/MainMethod/MainMethod/mathOP.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class mathOP
+    {
+        
+        public static int MathOperation(int Number1) // this is the first method that multiplies a given number
+        {
+            Number1 *= 10;
+            return Number1;
+        }
+
+        public static decimal MathOperation(decimal Number2) // this is the second method that adds a given number
+        {
+            Number2 += 1.71m;
+            return Number2;
+        }
+
+        public static int MathOperation(string Number3) // this is the third method that checks to make sure it is a string that can be a number and then divides that number.
+        {
+            if (Int32.TryParse(Number3, out int result))
+            {
+                result /= 4;
+                return result; 
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+    }
+}
