4d010eddc18c6ca099175e5477e90b56ef01ec42
TravisHeckel <Traheck@gmail.com>
2023-09-25 19:04:51 -0600

    update assignment for methodsubmission step 280

---
+++ Project and Assignments/MethodSubmission/MethodSubmission/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathOP multi = new mathOP(); //calls a instance of the class.
+
+            Console.WriteLine("PLease enter a number."); //this asks for the first number to be typed
+            string Answer1 =Console.ReadLine(); //saves first answer
+            
+            Console.WriteLine("Enter a number only if you want to, otherwise press enter (not necessary)."); //this asks for the second number to be typed.
+            string Answer2= Console.ReadLine(); // saves second answer.
+
+            Console.WriteLine("The final answer is " + mathOP.MathOperation(Answer1, Answer2)); //this calls the method and displays the result.
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Project and Assignments/MethodSubmission/MethodSubmission/mathOP.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class mathOP
+    {
+
+        public static int MathOperation(string Number1,  string Number2) 
+        {
+            int convertedNumber = Convert.ToInt32(Number1);  // this converts it to a int
+
+            if (!string.IsNullOrWhiteSpace(Number2)) //this checks to confirm whether the user gave a correct input
+            {
+                int convertedNumber2 = Convert.ToInt32(Number2); // this converts it to a int
+                int result = convertedNumber + convertedNumber2; //mathematic operation
+                return result;
+            }
+            else //this only returns if the user didn't give an input for Number2
+            {
+                int result = convertedNumber * 2;
+                return result;
+            }
+            
+
+        }
+
+    }
+}
