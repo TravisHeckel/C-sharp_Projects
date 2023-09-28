3a14510e34fe82cc59b356c5f4c71613ad574c71
TravisHeckel <Traheck@gmail.com>
2023-09-27 20:17:56 -0600

    moved assignment into correct folder

---
+++ Project and Assignments/Struct Assign/Struct Assign/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number one = new Number();
+            one.amount = 12.75m;
+            Console.WriteLine(one.amount);
+            Console.ReadLine();
+        }
+    }
+
+    public struct Number
+    {
+        public decimal amount { get; set; }
+    }
+}
