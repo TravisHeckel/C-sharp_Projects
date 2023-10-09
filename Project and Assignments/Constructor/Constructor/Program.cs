6b90a81cfa13118fdb12b68af536ea0b3afcaacd
TravisHeckel <Traheck@gmail.com>
2023-10-09 15:30:06 -0600

    update

--- Project and Assignments/Constructor/Constructor/Program.cs
+++ Project and Assignments/Constructor/Constructor/Program.cs
@@ -22,29 +22,6 @@ namespace Constructor
             var animal2 = "cat";
 
             Console.WriteLine(animal + " is better than " + animal2); //prints out instances items
-            //Warrior newWarrior = new Warrior(); //set up new warrior instance
-
-
-            //Console.WriteLine("what race are you?"); //ask for user input
-            //Console.WriteLine("ork"); //set options
-            //Console.WriteLine("elf"); //set options
-            //Console.WriteLine("robot"); //set options
-            //Console.WriteLine("human"); //set options
-            //string answer = Console.ReadLine(); //save user input
-
-            //newWarrior.race = answer; //save user input to the race variable in the class 
-
-
-            //Console.WriteLine("what weapon do you have?"); //ask for user input
-            //Console.WriteLine("laser"); //set options
-            //Console.WriteLine("knife"); //set options
-            //Console.WriteLine("club"); //set options
-            //Console.WriteLine("fist"); //set options
-            //string answer2 = Console.ReadLine(); // save user input
-
-            //newWarrior.weapon = answer2;  //save user input to the weapon variable in the class 
-
-            //Console.WriteLine(newWarrior.race + " " + newWarrior.weapon); //prints out the items chosen to the console.
 
             Console.ReadLine();
         }
